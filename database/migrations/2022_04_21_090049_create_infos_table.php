7e7c421d3af51fd580466c97ead51d92ce946b74
Diokou_Tech <diokoucheikh@gmail.com>
2022-04-23 12:44:53 +0000

    update final

---
+++ database/migrations/2022_04_21_090049_create_infos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('infos', function (Blueprint $table) {
+            $table->id();
+            $table->string('titre');
+            $table->text('description');
+            $table->string('lien')->unique();
+            $table->date('date_pub');
+            $table->string('date_for_human');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('infos');
+    }
+}
